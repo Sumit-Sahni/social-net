f09abe737997b43ba5ad8c9ef4c31f383c9b4603
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2023-03-19 12:53:42 +0530

    Changes

--- server.js
+++ server.js
@@ -57,7 +57,19 @@ app.use('/api/videos', videoRoutes);
 
 // _________________________________Deployment to Cyclic________________________________________
    
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 
 const PORT = process.env.PORT;  
