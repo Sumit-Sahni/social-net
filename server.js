88fb94840a0f53cecc54e2495353ec8f58390313
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2023-03-19 13:14:55 +0530

    Changes

--- server.js
+++ server.js
@@ -57,8 +57,8 @@ app.use('/api/videos', videoRoutes);
    
 // const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
