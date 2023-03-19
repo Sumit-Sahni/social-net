16deccb24b06bcb80e5ff685a991c2aabe1a762b
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2023-03-19 13:45:20 +0530

    Updates

--- backend/server.js
+++ backend/server.js
@@ -59,10 +59,10 @@ app.use('/api/videos', videoRoutes);
 // const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
